b80f10df9bb9eb5364606bb4459e60fdc2fe220a
Ana <ana.jane.mm94@gmail.com>
2022-02-22 18:18:15 +0100

    Siempre positivo

--- AppData/Local/Temp/Rar$DRa13184.18924/geometria-master/geometria/src/Circulo_AM.java
+++ AppData/Local/Temp/Rar$DRa13184.18924/geometria-master/geometria/src/Circulo_AM.java
@@ -7,6 +7,9 @@ public class Circulo extends FiguraGeometrica {
 	public Circulo(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
+                if (radio < 0){
+                    radio = radio*-1;
+                }
 	}
 
 	@Override
